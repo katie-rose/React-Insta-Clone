0208a664c1b0523ee44e96b222df1328847a3f66
Katie Rose <katiefelten@gmail.com>
2019-06-04 16:31:15 -0400

    added icons, fixed styling

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -3,20 +3,28 @@ import CommentSection from "../CommentSection/CommentSection";
 import "./PostContainer.css";
 
 const PostContainer = props => {
-    return(
-        <div className='post-container'>	      
- <div className='user-info'>
-                <img src={props.post.thumbnailUrl} alt='user thumbnail' className='user-thumbnail'/>
-                <p className='user-name'>{props.post.username}</p>
-            </div>
-                <img src={props.post.imageUrl} alt={props.post.username} className='post-image' />
-            <div className ='icons'>
-                <i className='far fa-heart' />
-                <i className='far fa-comment' />
-            </div> 
-            <b className='likes'>{props.post.likes} likes</b>
-            <CommentSection commentArray={props.post.comments}/>
-    </div>
-  );
+    return (
+      <div className="post-container">
+        <div className="user-info">
+          <img
+            src={props.post.thumbnailUrl}
+            alt="user thumbnail"
+            className="user-thumbnail"
+          />
+          <p className="user-name">{props.post.username}</p>
+        </div>
+        <img
+          src={props.post.imageUrl}
+          alt={props.post.username}
+          className="post-image"
+        />
+        <div className="post-icons">
+          <i className="far fa-heart" />
+          <i className="far fa-comment" />
+        </div>
+        <b className="post-likes">{props.post.likes} likes</b>
+        <CommentSection commentArray={props.post.comments} />
+      </div>
+    );
 };
 export default PostContainer;
